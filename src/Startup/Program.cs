cf62a9f417ae676c9130c8e9255b159d42f561ed
chriale <64493575+chriale@users.noreply.github.com>
2021-10-30 16:15:16 -0500

    Update Program.cs

--- src/Startup/Program.cs
+++ src/Startup/Program.cs
@@ -6,6 +6,7 @@ using Discord.Commands;
 using Discord.WebSocket;
 using Microsoft.Extensions.DependencyInjection;
 using Upland.InformationProcessor;
+using System.Timers;
 
 /*
 // ONLY UNCOMMENT FOR DEBUGING
@@ -17,6 +18,7 @@ using Upland.Types.Types;
 using System.Linq;
 using Upland.Infrastructure.Blockchain;
 using Upland.Types.BlockchainTypes;
+using Upland.Types;
 */
 
 class Program
@@ -25,6 +27,7 @@ class Program
     private CommandService _commands;
     private IServiceProvider _services;
     private InformationProcessor _informationProcessor;
+    private Timer _refreshTimer;
 
     /*
     static async Task Main(string[] args) // DEBUG FUNCTION
@@ -34,9 +37,10 @@ class Program
         InformationProcessor informationProcessor = new InformationProcessor();
         BlockchainRepository blockchainRepository = new BlockchainRepository();
 
-        string username;
-        string qualityLevel;
+        //string username;
+        //string qualityLevel;
         List<string> output = new List<string>();
+        //new Program().InitializeRefreshTimer();
 
         /// Test Optimizer
         //Console.Write("Enter the Upland Username: ");
@@ -54,22 +58,26 @@ class Program
         //output = await informationProcessor.GetSalesDataByCityId(1);
         //output = await informationProcessor.GetNeighborhoodPropertiesForSale(235, "Price", "All");
         //output = await informationProcessor.GetBuildingPropertiesForSale("City", 0, "price", "all");
+        //output = informationProcessor.GetNeighborhoodInformation("TXT"); 
         //await File.WriteAllTextAsync(@"C:\Users\chri1\Desktop\Upland\OptimizerBot\test_file.txt", string.Join(Environment.NewLine, output));
 
         // Populate CityProps And Neighborhoods
-        //await localDataManager.PopulateAllPropertiesInArea(40.656588, 40.492300, -74.031335, -74.264108, 8);
-        //localDataManager.DetermineNeighborhoodIdsForCity(8);
+        //await localDataManager.PopulateAllPropertiesInArea(40.656588, 40.492300, -74.031335, -74.264108, 8, true);
+        //foreach (int cityId in Consts.Cities.Keys) { localDataManager.DetermineNeighborhoodIdsForCity(cityId); }
 
         // Rebuild Property Structure List
         //await informationProcessor.RebuildPropertyStructures();
+        //await informationProcessor.RunCityStatusUpdate();
 
         //List<t3Entry> items = await blockchainRepository.GetActiveOffers();
     }
     */
     
+    
     static void Main(string[] args) 
         => new Program().RunBotAsync().GetAwaiter().GetResult();
     
+
     public async Task RunBotAsync()
     {
         _client = new DiscordSocketClient();
@@ -85,6 +93,8 @@ class Program
         string token = System.IO.File.ReadAllText(@"auth.txt");
 
         _client.Log += clientLog;
+
+        InitializeRefreshTimer();
 
         await RegisterCommandsAsync();
 
@@ -121,15 +131,102 @@ class Program
                 var result = await _commands.ExecuteAsync(context, argPos, _services);
                 if (!result.IsSuccess)
                 {
-                    Console.WriteLine(result.ErrorReason);
+                    Console.WriteLine(string.Format("{0}: {1}", string.Format("{0:MM/dd/yy H:mm:ss}", DateTime.Now), result.ErrorReason));
                     Console.WriteLine(string.Format("{0}: {1} - {2}", string.Format("{0:MM/dd/yy H:mm:ss}", DateTime.Now), message.Author.Username, message.Content));
-                    await context.Channel.SendMessageAsync(string.Format("ERROR: {0}{1}Contant Grombrindal.", result.ErrorReason, Environment.NewLine));
+                    switch(result.ErrorReason)
+                    {
+                        case "Unknown command.":
+                            await context.Channel.SendMessageAsync(string.Format("ERROR: I don't know that command. Try running !Help."));
+                            break;
+                        case "The input text has too many parameters.":
+                            await context.Channel.SendMessageAsync(string.Format("ERROR: There are too many parameters on that command. Try looking at the !Help documentation for that command."));
+                            break;
+                        case "The input text has too few parameters.":
+                            await context.Channel.SendMessageAsync(string.Format("ERROR: There are too few parameters on that command. Try looking at the !Help documentation for that command."));
+                            break;
+                        case "Failed to parse Int32.":
+                            await context.Channel.SendMessageAsync(string.Format("ERROR: Looks like you typed a word instead of a number. Try looking at the !Help documentation for that command."));
+                            break;
+                        case "Object reference not set to an instance of an object.":
+                        case "The server responded with error 503: ServiceUnavailable":
+                            await context.Channel.SendMessageAsync(string.Format("ERROR: Upland may be in Maintenance right now, try again later. If not something has gone horribly wrong, please contact Grombrindal."));
+                            break;
+                        case "The server responded with error 40005: Request entity too large":
+                            await context.Channel.SendMessageAsync(string.Format("ERROR: That file exceeds the size limit set discord unfortunaley. Try requesting it as a CSV instead."));
+                            break;
+                        default:
+                            await context.Channel.SendMessageAsync(string.Format("ERROR: Contant Grombrindal."));
+                            break;
+                    }
                 }
                 else
                 {
                     Console.WriteLine(string.Format("{0}: {1} - {2}", string.Format("{0:MM/dd/yy H:mm:ss}", DateTime.Now), message.Author.Username, message.Content));
                 }
             });
+        }
+    }
+
+    private void InitializeRefreshTimer()
+    {
+        _refreshTimer = new Timer();
+        _refreshTimer.Elapsed += (sender, e) =>
+        {
+            Task child = Task.Factory.StartNew(async () =>
+            {
+                await RunRefreshActions();
+            });
+        };
+        _refreshTimer.Interval = 36000000;
+        _refreshTimer.Start();
+    }
+
+    private async Task RunRefreshActions()
+    {
+        DateTime time = DateTime.Now;
+        if (time.Hour == 0) 
+        {
+            InformationProcessor informationProcessor = new InformationProcessor();
+
+            // Rebuild the structures list every night
+            try
+            {
+                Console.WriteLine(string.Format("{0}: Rebuilding Structures", string.Format("{0:MM/dd/yy H:mm:ss}", DateTime.Now)));
+               // await informationProcessor.RebuildPropertyStructures();
+                Console.WriteLine(string.Format("{0}: Rebuilding Complete", string.Format("{0:MM/dd/yy H:mm:ss}", DateTime.Now)));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(string.Format("{0}: Rebuilding Structures Failed: {1}", string.Format("{0:MM/dd/yy H:mm:ss}", DateTime.Now), ex.Message));
+            }
+
+            // Only Rebuild All on Sundays
+            if (time.DayOfWeek == DayOfWeek.Sunday)
+            {
+                try
+                {
+                    Console.WriteLine(string.Format("{0}: Refreshing All Cities", string.Format("{0:MM/dd/yy H:mm:ss}", DateTime.Now)));
+                    await informationProcessor.RunCityStatusUpdate(true);
+                    Console.WriteLine(string.Format("{0}: Refreshing All Cities Complete", string.Format("{0:MM/dd/yy H:mm:ss}", DateTime.Now)));
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(string.Format("{0}: Refreshing All Cities Failed: {1}", string.Format("{0:MM/dd/yy H:mm:ss}", DateTime.Now), ex.Message));
+                }
+            }
+            else
+            {
+                try
+                {
+                    Console.WriteLine(string.Format("{0}: Refreshing Non Sold Out Cities", string.Format("{0:MM/dd/yy H:mm:ss}", DateTime.Now)));
+                    await informationProcessor.RunCityStatusUpdate(false);
+                    Console.WriteLine(string.Format("{0}: Refreshing Non Sold Out Cities Complete", string.Format("{0:MM/dd/yy H:mm:ss}", DateTime.Now)));
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(string.Format("{0}: Refreshing Non Sold Out Cities Failed: {1}", string.Format("{0:MM/dd/yy H:mm:ss}", DateTime.Now), ex.Message));
+                }
+            }
         }
     }
 }
