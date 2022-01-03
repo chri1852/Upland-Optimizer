4402e62de061c48e8419c454fcc640e3e8b11361
chriale <64493575+chriale@users.noreply.github.com>
2022-01-03 17:16:13 -0600

    backend complete

--- src/Upland.InformationProcessor/MappingProcessor.cs
+++ src/Upland.InformationProcessor/MappingProcessor.cs
@@ -1,8 +1,12 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Drawing.Imaging;
+using System.Drawing.Text;
 using System.Linq;
 using Upland.Infrastructure.LocalData;
+using Upland.Types;
 using Upland.Types.Types;
 
 namespace Upland.InformationProcessor
@@ -22,7 +26,51 @@ namespace Upland.InformationProcessor
             BuildColorBlindKey();
         }
 
-        public void CreateSoldOutMap(int cityId, bool colorBlind, bool nonFSAOnly)
+        public Bitmap CreateMap(int cityId, string type, bool colorBlind)
+        {
+            string typeString;
+            Bitmap map;
+            Bitmap key;
+
+            if (type.ToUpper() == "SOLD")
+            {
+                typeString = "Sold Out";
+                map = CreateSoldOutMap(cityId, colorBlind, false);
+                key = BuildStandardKey(colorBlind);
+            }
+            else if (type.ToUpper() == "SOLDNONFSA")
+            {
+                typeString = "Sold Out Non FSA";
+                map = CreateSoldOutMap(cityId, colorBlind, true);
+                key = BuildStandardKey(colorBlind);
+            }
+            else
+            {
+                throw new Exception("Invalid Map Type Selected");
+            }
+
+            Bitmap combinedMap = new Bitmap(map.Width + key.Width, map.Height + 60);
+
+            Bitmap header = CreateHeader(cityId, typeString, combinedMap.Width);
+            Bitmap footer = CreateFooter(combinedMap.Width);
+
+            using (Graphics g = Graphics.FromImage(combinedMap))
+            {
+                using (SolidBrush brush = new SolidBrush(Color.FromArgb(255, 255, 255)))
+                {
+                    g.FillRectangle(brush, 0, 0, combinedMap.Width, combinedMap.Height);
+                }
+                g.DrawImage(header, 0, 0);
+                g.DrawImage(map, 0, header.Height + 1);
+                g.DrawImage(key, map.Width+1, header.Height + 1);
+                g.DrawImage(footer, 0, combinedMap.Height + - 25);
+                g.Flush();
+            }
+
+            return combinedMap;
+        }
+
+        private Bitmap CreateSoldOutMap(int cityId, bool colorBlind, bool nonFSAOnly)
         {
             Bitmap testMap = LoadBlankMapByCityId(cityId);
 
@@ -53,49 +101,49 @@ namespace Upland.InformationProcessor
                             ? neighborhoodStats[colorDictionary[actualColor].Id].PercentNonFSAMinted
                             : neighborhoodStats[colorDictionary[actualColor].Id].PercentMinted;
 
-                        if (percentMinted >= 0 && percentMinted < 10)
+                        if (percentMinted == 100)
                         {
-                            newBitmap.SetPixel(i, j, colorKeys[0]);
+                            newBitmap.SetPixel(i, j, colorKeys[10]);
                         }
-                        else if (percentMinted >= 10 && percentMinted < 20)
-                        {
-                            newBitmap.SetPixel(i, j, colorKeys[1]);
-                        }
-                        else if (percentMinted >= 20 && percentMinted < 30)
-                        {
-                            newBitmap.SetPixel(i, j, colorKeys[2]);
-                        }
-                        else if (percentMinted >= 30 && percentMinted < 40)
-                        {
-                            newBitmap.SetPixel(i, j, colorKeys[3]);
-                        }
-                        else if (percentMinted >= 40 && percentMinted < 50)
-                        {
-                            newBitmap.SetPixel(i, j, colorKeys[4]);
-                        }
-                        else if (percentMinted >= 50 && percentMinted < 60)
-                        {
-                            newBitmap.SetPixel(i, j, colorKeys[5]);
-                        }
-                        else if (percentMinted >= 60 && percentMinted < 70)
-                        {
-                            newBitmap.SetPixel(i, j, colorKeys[6]);
-                        }
-                        else if (percentMinted >= 70 && percentMinted < 80)
-                        {
-                            newBitmap.SetPixel(i, j, colorKeys[7]);
-                        }
-                        else if (percentMinted >= 80 && percentMinted < 90)
-                        {
-                            newBitmap.SetPixel(i, j, colorKeys[8]); ;
-                        }
-                        else if (percentMinted >= 90 && percentMinted < 100)
+                        else if (percentMinted >= 90)
                         {
                             newBitmap.SetPixel(i, j, colorKeys[9]);
                         }
+                        else if (percentMinted >= 80)
+                        {
+                            newBitmap.SetPixel(i, j, colorKeys[8]);
+                        }
+                        else if (percentMinted >= 70)
+                        {
+                            newBitmap.SetPixel(i, j, colorKeys[7]);
+                        }
+                        else if (percentMinted >= 60)
+                        {
+                            newBitmap.SetPixel(i, j, colorKeys[6]);
+                        }
+                        else if (percentMinted >= 50)
+                        {
+                            newBitmap.SetPixel(i, j, colorKeys[5]);
+                        }
+                        else if (percentMinted >= 40)
+                        {
+                            newBitmap.SetPixel(i, j, colorKeys[4]);
+                        }
+                        else if (percentMinted >= 30)
+                        {
+                            newBitmap.SetPixel(i, j, colorKeys[3]);
+                        }
+                        else if (percentMinted >= 20)
+                        {
+                            newBitmap.SetPixel(i, j, colorKeys[2]);
+                        }
+                        else if (percentMinted >= 10)
+                        {
+                            newBitmap.SetPixel(i, j, colorKeys[1]);
+                        }
                         else
                         {
-                            newBitmap.SetPixel(i, j, colorKeys[10]);
+                            newBitmap.SetPixel(i, j, colorKeys[0]);
                         }
                     }
                     else
@@ -105,8 +153,124 @@ namespace Upland.InformationProcessor
                 }
             }
 
-            // TODO THIS SHOULD RETURN A BITMAP
-            SaveMap(newBitmap, "test123");
+            return newBitmap;
+        }
+
+        private Bitmap BuildStandardKey(bool colorBlind)
+        {
+            List<string> keyTextStrings = new List<string>();
+            keyTextStrings.Add("     > 10%");
+            keyTextStrings.Add("10% -- 20%");
+            keyTextStrings.Add("20% -- 30%");
+            keyTextStrings.Add("30% -- 40%");
+            keyTextStrings.Add("40% -- 50%");
+            keyTextStrings.Add("50% -- 60%");
+            keyTextStrings.Add("60% -- 70%");
+            keyTextStrings.Add("70% -- 80%");
+            keyTextStrings.Add("80% -- 90%");
+            keyTextStrings.Add("90% -- 99%");
+            keyTextStrings.Add("Sold Out");
+
+            return BuildKey(colorBlind, keyTextStrings);
+        }
+
+        private Bitmap BuildKey(bool colorBlind, List<string> keyTextStrings)
+        {
+            List<Color> colorKeys = colorBlind ? _colorBlindKey : _standardKey;
+
+            Bitmap bmp = new Bitmap((keyTextStrings.Max(t => t.Length) * 14) + 4, 279);
+            Graphics g = Graphics.FromImage(bmp);
+            Font font = new Font("Consolas", 20, GraphicsUnit.Pixel);
+            StringFormat format = new StringFormat()
+            {
+                Alignment = StringAlignment.Center,
+                LineAlignment = StringAlignment.Center
+            };
+
+            using (SolidBrush brush = new SolidBrush(Color.FromArgb(0, 0, 0)))
+            {
+                g.FillRectangle(brush, 0, 0, bmp.Width, 2);
+                g.FillRectangle(brush, 0, 0, 2, bmp.Height);
+                g.FillRectangle(brush, bmp.Width-2, 0, bmp.Width, bmp.Height);
+                g.FillRectangle(brush, 0, bmp.Height-2, bmp.Width, bmp.Height);
+            }
+
+            for (int i = 0; i < colorKeys.Count; i++)
+            {
+                RectangleF rectf = new RectangleF(2, (i * 25) + 2, bmp.Width - 4, 25);
+                using (SolidBrush brush = new SolidBrush(colorKeys[i]))
+                {
+                    g.FillRectangle(brush, rectf);
+                }
+
+                g.DrawString(keyTextStrings[i], font, i < 7 ? Brushes.Black : Brushes.White, rectf, format);
+            }
+
+            g.Flush();
+
+            return bmp;
+        }
+
+        private Bitmap CreateHeader(int cityId, string type, int width)
+        {
+            string headerString = Consts.Cities[cityId];
+            headerString += " - ";
+            headerString += type;
+            headerString += " - ";
+            headerString += string.Format("{0:MM/dd/yyyy HH:mm:ss zzz}", DateTime.UtcNow);
+
+            Bitmap bmp = new Bitmap(width, 35);
+
+            RectangleF rectf = new RectangleF(0, 0, bmp.Width, bmp.Height);
+
+            // Create graphic object that will draw onto the bitmap
+            Graphics g = Graphics.FromImage(bmp);
+
+            StringFormat format = new StringFormat()
+            {
+                Alignment = StringAlignment.Center,
+                LineAlignment = StringAlignment.Center
+            };
+
+            using (SolidBrush brush = new SolidBrush(Color.FromArgb(255, 255, 255)))
+            {
+                g.FillRectangle(brush, 0, 0, bmp.Width, bmp.Height);
+            }
+
+            g.DrawString(headerString, new Font("Consolas", 30, GraphicsUnit.Pixel), Brushes.Black, rectf, format);
+
+            g.Flush();
+
+            return bmp;
+        }
+
+        private Bitmap CreateFooter(int width)
+        {
+            string footerString = "Autogenerated by the Upland Optimizer Bot - https://discord.gg/hsrxQb7UQb";
+
+            Bitmap bmp = new Bitmap(width, 25);
+
+            RectangleF rectf = new RectangleF(0, 0, bmp.Width, bmp.Height);
+
+            // Create graphic object that will draw onto the bitmap
+            Graphics g = Graphics.FromImage(bmp);
+
+            StringFormat format = new StringFormat()
+            {
+                Alignment = StringAlignment.Center,
+                LineAlignment = StringAlignment.Center
+            };
+
+            using (SolidBrush brush = new SolidBrush(Color.FromArgb(255, 255, 255)))
+            {
+                g.FillRectangle(brush, 0, 0, bmp.Width, bmp.Height);
+            }
+
+            g.DrawString(footerString, new Font("Consolas", 20, GraphicsUnit.Pixel), Brushes.Black, rectf, format);
+
+            g.Flush();
+
+            return bmp;
         }
 
         private Bitmap LoadBlankMapByCityId(int cityId)
